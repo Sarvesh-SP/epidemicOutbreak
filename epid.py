ac3276c40d85a3c952e61596abcc0a35a74bd532
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-04-30 08:24:08 +0530

    Added UI functionality

--- epid.py
+++ epid.py
@@ -7,16 +7,24 @@ class Simulation():
     def __init__(self):
 
         self.day_number = 1
-
+        print("To simulate an epidemic outbreak, we must know the population size.")
         self.pop_size = int(input("---Enter the Population size: "))
-
+        print("\nWe must first start by infecting a portion of the population.")
         self.inf_percentage = float(
-            input("---Enter the Infection Percentage: "))
+            input("---Enter the Percentage(0 - 100) of the population to initially infect: "))
         self.inf_percentage /= 100
-        self.inf_pro = float(input("---Enter the Infection Probability: "))
+        print("\nWe must know th risk a person has to contract the disease when exposed.")
+        self.inf_pro = float(input(
+            "---Enter the Probability(0 - 100) that a person gets infected when exposed to the disease: "))
 
+        print("\nWe must know how long the infection will last when exposed.")
+        self.inf_duration = int(
+            input("---Enter the duration (in days) of the infection: "))
+
+        print("\nWe must know th mortality rate of those infected.")
         self.mor_rate = float(input("---Enter the mortality rate: "))
 
+        print("\nWe must know how long to run the simulation.")
         self.sim_days = int(input("---Enter the days to simulate: "))
 
 
@@ -44,7 +52,7 @@ class Person():
             self.days_infected += 1
             if random.randint(0, 100) < sim.mor_rate:
                 self.die()
-            elif self.days_infected == sim.sim_days:
+            elif self.days_infected == sim.inf_duration:
                 self.heal()
 
 
@@ -59,7 +67,7 @@ class Population():
             self.population.append(person)
 
     def initial_infection(self, sim):
-        infected_count = int(round(sim.inf_percentage * sim.pop_size), 0)
+        infected_count = int(round(sim.inf_percentage*sim.pop_size, 0))
 
         for x in range(infected_count):
             self.population[x].is_infected = True
@@ -104,5 +112,41 @@ class Population():
         print(f"----Total People Infected: {total_infC} / {sim.pop_size}")
         print(f"----Total Deaths: {total_dedC} / {sim.pop_size}")
 
-    def graphics():
-        pass
+    def graphics(self):
+        """A Graphical representation for a population. O is healthy, I is Infected, X is dead."""
+
+        status = []
+        for person in self.population:
+            if person.is_dead:
+                char = 'X'
+            elif person.is_infected:
+                char = 'I'
+            else:
+                char = 'O'
+            status.append(char)
+
+        for x in status:
+            print(x, end="-")
+
+
+sim = Simulation()
+pop = Population(sim)
+
+pop.initial_infection(sim)
+pop.display_statistics(sim)
+
+
+pop.graphics()
+
+
+input("\nPress enter to begin the simulation")
+
+
+for x in range(1, sim.sim_days):
+    pop.spread_infection(sim)
+    pop.update(sim)
+    pop.display_statistics(sim)
+    pop.graphics()
+
+    if x != sim.sim_days - 1:
+        input("\nPress enter to advance to the next day.")
